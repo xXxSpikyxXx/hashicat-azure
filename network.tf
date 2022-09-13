51531371f8be87eac60e01722a7af8ab1c4ae493
stevenng <steven_ng@pcs-security.com>
2022-09-13 16:48:36 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,8 +1,3 @@
-module "network" {
-  source  = "app.terraform.io/PCSSecurity/network/azurerm"
-  version = "3.5.0"
-  # insert required variables here
-
 provider "azurerm" {
   features {}
 }
@@ -13,7 +8,8 @@ resource "azurerm_resource_group" "myresourcegroup" {
 }
 
 module "network" {
-  source              = "Azure/network/azurerm"
+  source              = "app.terraform.io/PCSSecurity/network/azurerm"
+  version = "3.5.0"
   resource_group_name = azurerm_resource_group.example.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]

a4030fdf29b85334eeeac37faf65679ebfdc3b40
stevenng <steven_ng@pcs-security.com>
2022-09-13 16:41:17 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -3,6 +3,9 @@ module "network" {
   source  = "app.terraform.io/PCSSecurity/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "stevenng-workshop"
+resource "azurerm_resource_group" "myresourcegroup" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
 }
+resource_group_name = azurerm_resource_group.myresourcegroup.name
 }

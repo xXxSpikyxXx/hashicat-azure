d3b1a2cf09033641dea0c9656f4d24462abb4b94
stevenng <steven_ng@pcs-security.com>
2022-09-13 15:53:44 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "Azure/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "myresourcegroup"
+  resource_group_name = "stevenng-workshop"
 }
